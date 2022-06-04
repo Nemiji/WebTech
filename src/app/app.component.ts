32df6b0aeedce79429fdfc312085fb53cc76ae2f
Janosch <janosch-git>
2022-06-04 16:24:47 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'web-app';
+export class AppComponent implements OnInit {
+
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  showNavigationArrows = true;
+  showNavigationIndicators = true;
+
+  
+
+  @ViewChild('ngcarousel', { static: true }) ngCarousel!: NgbCarousel;
+  ngOnInit() {}
+  // Move to specific slide
+  navigateToSlide(item: any) {
+    this.ngCarousel.select(item);
+    console.log(item);
+  }
+  // Move to previous slide
+  getToPrev() {
+    this.ngCarousel.prev();
+  }
+  // Move to next slide
+  goToNext() {
+    this.ngCarousel.next();
+  }
+  // Pause slide
+  stopCarousel() {
+    this.ngCarousel.pause();
+  }
+  // Restart carousel
+  restartCarousel() {
+    this.ngCarousel.cycle();
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdDropdownBasic } from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdDropdownBasic
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
